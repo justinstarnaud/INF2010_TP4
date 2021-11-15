a339c9fd4d8e8f7f9b788253111cd27866b8b1a2
Eduardo <eduardofalluh@gmail.com>
2021-11-15 16:43:18 -0500

    bien avancer

--- src/Heap.java
+++ src/Heap.java
@@ -17,7 +17,7 @@ public class Heap<ValueType extends Comparable<? super ValueType>> implements It
         public Heap(boolean isMax, Collection<ValueType> elements){
             // Ne pas modifier ces lignes
             this.isMax = isMax;
-            this.elements = new ArrayList<>(elements);
+            this.elements = new ArrayList<>();
             this.elements.add(null);
             this.elements.addAll(elements);
             // Ne pas modifier ces lignes
@@ -28,7 +28,8 @@ public class Heap<ValueType extends Comparable<? super ValueType>> implements It
 
         /* TODO Implementer le compare pour un MaxHeap et MinHeap */
         protected boolean compare(ValueType first, ValueType second){
-            return first.compareTo(second) > 0  ? true : false;
+            if(isMax){return first.compareTo(second) > 0  ? true : false;}
+            return first.compareTo(second) < 0  ? true : false;
         }
 
         /* TODO Retourner l'index du parent */
@@ -53,7 +54,9 @@ public class Heap<ValueType extends Comparable<? super ValueType>> implements It
 
         /* TODO Constuire le monceau avec les noeuds dans "elements" */
         public void buildHeap(){
-            
+            for(int i = 1; i < this.size(); i++){
+                percolateDown(i);
+            }
         }
 
         /* TODO Echanger les elements qui se retrouve aux indexes currentIndex et parentIndex */
@@ -65,50 +68,90 @@ public class Heap<ValueType extends Comparable<? super ValueType>> implements It
 
         /* TODO Ajouter un element dans le monceaux. */
         public void insert(ValueType value){
-            this.size();
-            this.elements.insert();
-            
+            this.elements.add(value);
+            int index = this.size()-1;
+            boolean faireComparaison = true;
+            while(index != 1 && faireComparaison){
+                faireComparaison = this.compare(value, this.elements.get(this.parentIndex(index)));
+                if(faireComparaison){
+                    this.swap(index, this.parentIndex(index));
+                }
+                index = this.parentIndex(index);
+            }
         }
 
         /* TODO Completer l'implementation des conditions de percolateDown pour un heap */
         private void percolateDown(int index){
             int child;
             ValueType temp = elements.get(index);
-            for(; index * 2 < size() / 2; index = child){
+            for(; index * 2 < size() ; index = child){
 
                 child = index * 2;
 
-                if(child != size() && array[child + 1 ].compareTo(array[child]) < 0){/* TODO Ajouter une condition pour evaluer les deux noeuds */) {
+                if(child != size() && this.compare( this.elements.get(child + 1), this.elements.get(child) )){/* TODO Ajouter une condition pour evaluer les deux noeuds */
                     child++;
                 }
 
-                if(true && array[child].compareTo(temp) < 0){/*TODO Ajouter une condition pour evaluer les deux noeuds */){
+                if(this.compare(this.elements.get(child), temp)){ /*TODO Ajouter une condition pour evaluer les deux noeuds */
                     elements.set(index, elements.get(child));
                 }
-                else
+                else{
                     break;
+                }
             }
             elements.set(index, temp);
         }
 
         /* TODO En utilisant leftChildIndex, ajouter les elements gauche du Heap dans une liste et les retourner. */
         public List<ValueType> getLeftElements(){
-            return new ArrayList<Integer>(heap.add(leftChildIndex));
+            List<ValueType> leftList = new ArrayList<ValueType>();
+            int exposant = 1;
+            while(Math.pow(2, exposant) < this.size()){
+                for(int i = (int) Math.pow(2, exposant); i < Math.pow(2, exposant) + Math.pow(2, exposant -1); i++){
+                    if(i < this.size()){
+                        leftList.add(this.elements.get(i));
+                    }
+                }
+                exposant++;
+            }
+            return leftList;
         }
 
         /* TODO En utilisant rightChildIndex, ajouter les droites du Heap dans une liste et les retourner. */
         public List<ValueType> getRightElements(){
-            return new ArrayList<Integer>(heap.add(rightChildIndex));
+            List<ValueType> rightList = new ArrayList<ValueType>();
+            int exposant = 1;
+            while(Math.pow(2, exposant) + Math.pow(2, exposant -1) < this.size()){
+                for(int i = (int) (Math.pow(2, exposant) + Math.pow(2, exposant -1)); i < Math.pow(2, exposant +1); i++){
+                    if(i < this.size()){
+                        rightList.add(this.elements.get(i));
+                    }
+                }
+                exposant++;
+            }
+            return rightList;
         }
 
         /* TODO En utilisant parentIndex, ajouter les noeuds  parents du Heap dans une liste et les retourner. */
         public List<ValueType> getParentElements(){
-            return  new ArrayList<Integer>(heap.add(parentIndex));
+            List<ValueType> parentList = new ArrayList<ValueType>();
+            int index = 1;
+            while(2 * index < this.size()){
+                parentList.add(this.elements.get(index));
+                index++;
+            }
+            return parentList;
         }
 
         /* TODO Ajouter les noeuds feuilles du monceau en utilisant isLeaf */
         public List<ValueType> getLeaves(){
-            return new ArrayList<>(heap.isLeaf()); //comprend pas comment get le/les derniers elements dans les branches de l'arbre
+            List<ValueType> leafList = new ArrayList<ValueType>();
+            int index = this.size() -1;
+            while(isLeaf(index)){
+                leafList.add(this.elements.get(index));
+                index--;
+            }
+            return leafList;
         }
 
 }
